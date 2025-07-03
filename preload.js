a2c55edc61cfd1c2d038d8741cd2553b3157127f
MarineGoncalves <marinecg2@gmail.com>
2025-07-03 10:49:21 -0300

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge } = require('electron');
-console.log("Preload carregado! 🚀");
-
-contextBridge.exposeInMainWorld('api', {
-    verElectron: () => process.versions.electron
-});
-
